73a4a50bf825a99c12d204be7ba6ecc80aba7dcd
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-29 17:30:45 -0300

    :wrench: chore: initial configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-1": "#343e3d",
+        "primary-2": "#607466",
+        "primary-2-dark": "#516156",
+        "primary-3": "#aedcc0",
+        "primary-3-dark": "#7f9f8b",
+        "primary-4": "#7bd389",
+        "primary-5": "#38e4ae",
+        "primary-6": "#3f4b3b",
+        "primary-7": "#44633f",
+        "primary-8": "#5a9367",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
