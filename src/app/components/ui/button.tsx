aaf001a2a12ef25a7b92608dd41a20e7ad069acc
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-29 21:19:14 -0300

    :sparkles: feat: Update button and input components

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -13,8 +13,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={twMerge(
           ` ${
             variant === "button" &&
-            "flex cursor-pointer items-center justify-center rounded-lg bg-primary-2 px-3 py-1.5 font-medium transition duration-300 hover:bg-primary-3-dark/70 "
-          } ${className}`,
+            " rounded-lg bg-primary-2 px-3 py-1.5 font-medium  hover:bg-primary-3-dark/70 "
+          } ${className} flex cursor-pointer items-center justify-center transition duration-300`,
         )}
         ref={ref}
         {...props}
--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -1,4 +1,6 @@
-import { ComponentPropsWithoutRef, forwardRef } from "react";
+"use client";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { ComponentPropsWithoutRef, forwardRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface InputProps extends ComponentPropsWithoutRef<"input"> {
@@ -7,18 +9,48 @@ interface InputProps extends ComponentPropsWithoutRef<"input"> {
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, error, errorMessage, ...props }, ref) => {
+  ({ className, error, errorMessage, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState<"password" | "text">(
+      "password",
+    );
+    const handleShowPassword = () => {
+      setShowPassword(showPassword === "text" ? "password" : "text");
+    };
+
     const inputClassName = twMerge(
       className,
-      " placeholder-opacity-70 outline-none transition-all px-2",
-      error ? "border-red-500" : "focus:ring-1 focus:ring-primary-3-dark",
+      " outline-none transition-all pl-2 pr-2 placeholder-primary-3/40 placeholder-opacity-50 border",
+      error
+        ? "border-red-500"
+        : "focus:ring-1 focus:ring-primary-3-dark border-transparent",
     );
 
     return (
       <div className="flex h-full w-full flex-col items-center">
-        <input ref={ref} className={inputClassName} {...props} />
+        <div className="relative flex w-full">
+          <input
+            ref={ref}
+            className={`${inputClassName} ${type === "password" && "!pr-8"}`}
+            {...props}
+            type={type !== "password" ? type : showPassword}
+          />
+          {type === "password" && (
+            <div
+              className="focus-visible:ring-ring absolute	 right-2 top-1/2 -translate-y-1/2 transform cursor-pointer focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50"
+              onClick={handleShowPassword}
+            >
+              {showPassword === "password" ? (
+                <EyeOffIcon width={20} height={20} />
+              ) : (
+                <EyeIcon width={20} height={20} />
+              )}
+            </div>
+          )}
+        </div>
         {error && errorMessage && (
-          <span className="mt-1 text-xs text-red-500 ">{errorMessage}</span>
+          <span className="text-error mt-1 flex w-full items-center justify-start text-xs ">
+            {errorMessage}
+          </span>
         )}
       </div>
     );
@@ -41,7 +73,7 @@ export default Input;
 //     <div className="flex w-full flex-col">
 //       <input ref={ref} className={inputClassName} {...props} />
 //       {error && errorMessage && (
-//         <span className="mt-1 text-xs text-red-500 ">{errorMessage}</span>
+//         <span className="mt-1 text-xs text-error ">{errorMessage}</span>
 //       )}
 //     </div>
 //   );
