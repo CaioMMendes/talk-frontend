06acd8ef734597a69fd218af4191d0d67da95214
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-30 02:09:01 -0300

    :sparkles: feat: Add get time function

---
+++ src/app/utils/get-time.ts
@@ -0,0 +1,14 @@
+export default function getTime() {
+  let hours = String(new Date().getHours());
+  let minutes = String(new Date().getMinutes());
+  if (Number(minutes) < 10) {
+    minutes = "0" + minutes;
+  }
+  if (Number(hours) < 10) {
+    hours = "0" + hours;
+  }
+
+  const time = `${hours}:${minutes}`;
+
+  return { time };
+}
